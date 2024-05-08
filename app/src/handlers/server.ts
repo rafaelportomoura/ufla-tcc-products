42025972a3281c61e1d63676585e0993ca898b8d
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-08 20:38:42 -0300

    refactor: some changes but fastify is worst than express (Don't believe in frameworks marketing)

--- app/src/handlers/server.ts
+++ app/src/handlers/server.ts
@@ -7,29 +7,31 @@ import { StatusCodes } from 'http-status-codes';
 import { CONFIGURATION } from '../constants/configuration';
 import { router } from '../routes';
 
-const server = Fastify({
-  logger: {
-    level: 'silent'
-  }
-});
-server.register(qs, {});
-server.register(multipart);
-
-server.get('/health-check', (_, res) => res.status(StatusCodes.OK).send('alive'));
-
-server.register(router);
-
-server.listen(
-  {
-    port: CONFIGURATION.PORT,
-    host: '0.0.0.0'
-  },
-  (err, addr) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
+(async () => {
+  const server = Fastify({
+    logger: {
+      level: 'silent'
     }
+  });
+  await server.register(qs, {});
+  await server.register(multipart);
 
-    console.info(`RUNNING ON PORT ${addr}`);
-  }
-);
+  server.get('/health-check', (_, res) => res.status(StatusCodes.OK).send('alive'));
+
+  await server.register(router);
+
+  server.listen(
+    {
+      port: CONFIGURATION.PORT,
+      host: '0.0.0.0'
+    },
+    (err, addr) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+
+      console.info(`RUNNING ON PORT ${addr}`);
+    }
+  );
+})();
