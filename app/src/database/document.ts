9a66c72025e71420f3a070974bad6ba5cf314bb7
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-08 14:15:56 -0300

    fix: encode uri

--- app/src/database/document.ts
+++ app/src/database/document.ts
@@ -44,7 +44,7 @@ export class DocumentDatabase {
 
       const query = !isEmpty(options) ? `?${qs.stringify(options)}` : '';
 
-      const uri = encodeURI(`${protocol}://${username}:${password}@${host}${query}`);
+      const uri = `${protocol}://${username}:${encodeURIComponent(password)}@${host}${query}`;
 
       await mongoose.connect(uri, {
         dbName: database,
