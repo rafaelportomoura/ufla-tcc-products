e02b8457ee4b3783773da19202f0a6ae7d0b44e6
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-08 21:40:09 -0300

    refactor: improve validationError

--- app/src/exceptions/ValidationError.ts
+++ app/src/exceptions/ValidationError.ts
@@ -2,9 +2,11 @@ import { CODE_MESSAGES } from '../constants/codeMessages';
 import { BadRequestError } from './BadRequestError';
 
 export class ValidationError extends BadRequestError {
-  constructor(public issues: unknown) {
+  issues: unknown;
+  constructor(issues: unknown) {
     super(CODE_MESSAGES.VALIDATION_ERROR);
     this.name = 'ValidationError';
+    this.issues = issues;
   }
 
   toJSON() {
